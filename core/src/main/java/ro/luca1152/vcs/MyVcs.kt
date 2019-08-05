cecf8a2913f50ee5055b24474365e1b568c70990
Luca1152 <t.luca1152@gmail.com>
2019-08-05 13:21:49 +0300

    Initial commit
    Created the initial project.

---
+++ core/src/main/java/ro/luca1152/vcs/MyVcs.kt
@@ -0,0 +1,5 @@
+package ro.luca1152.vcs
+
+import com.badlogic.gdx.ApplicationAdapter
+
+class MyVcs : ApplicationAdapter()
---
+++ desktop/src/main/java/ro/luca1152/vcs/desktop/DesktopLauncher.kt
@@ -0,0 +1,31 @@
+package ro.luca1152.vcs.desktop
+
+import com.badlogic.gdx.Files.FileType
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import ro.luca1152.vcs.MyVcs
+
+/** Launches the desktop (LWJGL) application.  */
+object DesktopLauncher {
+
+    private val defaultConfiguration: LwjglApplicationConfiguration
+        get() {
+            val configuration = LwjglApplicationConfiguration()
+            configuration.title = "vcs"
+            configuration.width = 640
+            configuration.height = 480
+            for (size in intArrayOf(128, 64, 32, 16)) {
+                configuration.addIcon("libgdx$size.png", FileType.Internal)
+            }
+            return configuration
+        }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        createApplication()
+    }
+
+    private fun createApplication(): LwjglApplication {
+        return LwjglApplication(MyVcs(), defaultConfiguration)
+    }
+}
